2c8677d789eb0119af9f458857a00a6ebc770cf2
Saleem Hadad <saleem.hadad@talabat.com>
2022-06-25 07:48:51 +0400

    wip

--- resources/views/vendor/larecipe/partials/sidebar.blade.php
+++ resources/views/vendor/larecipe/partials/sidebar.blade.php
@@ -1,5 +1,5 @@
 <div class="sidebar pt-0" :class="[{'is-hidden': ! sidebar}]">
-    <div class="flex items-center justify-center">
+    <div class="pt-6 flex items-center justify-center">
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2975960653015025"
         crossorigin="anonymous"></script>
     <!-- docs-toc -->

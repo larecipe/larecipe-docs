79f2f058ed853711c43a49dc4757c440968b3abd
Saleem Hadad <saleem.hadad@talabat.com>
2022-06-25 07:47:30 +0400

    wip

--- resources/views/vendor/larecipe/partials/sidebar.blade.php
+++ resources/views/vendor/larecipe/partials/sidebar.blade.php
@@ -1,3 +1,15 @@
 <div class="sidebar pt-0" :class="[{'is-hidden': ! sidebar}]">
+    <div>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2975960653015025"
+        crossorigin="anonymous"></script>
+    <!-- docs-toc -->
+    <ins class="adsbygoogle"
+        style="display:inline-block;width:200px;height:200px"
+        data-ad-client="ca-pub-2975960653015025"
+        data-ad-slot="9770327392"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+    </div>
     {!! $index !!}
 </div>

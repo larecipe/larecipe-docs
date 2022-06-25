f8c0cad0b475e8204dc31d733f8acd99884b2925
Saleem Hadad <saleem.hadad@talabat.com>
2022-06-25 07:42:20 +0400

    wip

--- resources/views/vendor/larecipe/docs.blade.php
+++ resources/views/vendor/larecipe/docs.blade.php
@@ -6,6 +6,21 @@
 	
 	<div class="documentation is-{{ config('larecipe.ui.code_theme') }}" :class="{'expanded': ! sidebar}">
         {!! $content !!}
+
+		<div>
+		<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2975960653015025"
+			crossorigin="anonymous"></script>
+		<!-- docs-footer -->
+		<ins class="adsbygoogle"
+			style="display:block"
+			data-ad-client="ca-pub-2975960653015025"
+			data-ad-slot="3218458963"
+			data-ad-format="auto"
+			data-full-width-responsive="true"></ins>
+		<script>
+			(adsbygoogle = window.adsbygoogle || []).push({});
+		</script>
+		</div>
 	</div>
 </div>
 @endsection

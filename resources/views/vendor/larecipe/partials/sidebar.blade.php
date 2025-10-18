b7bf30b3941daabe976d1ab2c1718c86960407c3
Saleem Hadad <saleem_ha@hotmail.com>
2025-10-18 23:40:16 +0400

    remove ads

--- resources/views/vendor/larecipe/partials/sidebar.blade.php
+++ resources/views/vendor/larecipe/partials/sidebar.blade.php
@@ -1,13 +1,3 @@
 <div class="sidebar pt-0" :class="[{'is-hidden': ! sidebar}]">
-    <div class="pt-6 flex items-center justify-center">
-    <!-- docs-toc -->
-    <ins class="adsbygoogle"
-        style="display:inline-block;width:200px;height:200px"
-        data-ad-client="ca-pub-2975960653015025"
-        data-ad-slot="9770327392"></ins>
-    <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
-    </div>
     {!! $index !!}
 </div>
